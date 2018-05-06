4ed3df368e638eff30c961a7461d0abfce070c4c
Le Thanh Tu <t.le@shopmacher.de>
2018-05-06 10:56:16 +0700

    second-commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var contactForm = document.getElementById('#contactForm');
+console.log(contactForm);
+
+
+
+
+var contactForm2 = document.getElementById('#contactForm');
+console.log(contactForm2);
