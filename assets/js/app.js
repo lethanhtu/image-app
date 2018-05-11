0a18769d5d375c77cf47aa85d403577455246b83
Le Thanh Tu <t.le@shopmacher.de>
2018-05-11 18:09:54 +0700

    commit

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-var contactForm = document.getElementById('#contactForm');
-console.log(contactForm);
-
-
-
-
-var contactForm2 = document.getElementById('#contactForm');
-console.log(contactForm2);
---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HomepageController extends Controller
+{
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
